2078fd9f8742c6c2e0a57e4dde00b1caf2dc6254
KT20201224 <rudxo0525@gmail.com>
2024-10-21 09:15:20 +0900

    refactor: dto package changed

--- src/main/java/me/kyoungtae/Blog/controller/BlogApiController.java
+++ src/main/java/me/kyoungtae/Blog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.kyoungtae.Blog.controller;
 import lombok.RequiredArgsConstructor;
 import me.kyoungtae.Blog.domain.Article;
 import me.kyoungtae.Blog.dto.AddArticleRequest;
+import me.kyoungtae.Blog.dto.ArticleResponse;
 import me.kyoungtae.Blog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,4 +27,17 @@ public class BlogApiController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+
 }
